0c1522747669f33ce0ef1abc290edadae8339cdd
Connor Cowan <ccowan003@comcast.net>
2019-07-05 21:30:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,13 @@ class Song
     @name = name
   end
   #sets the artist object to belong to the song
+  def self.new_by_filename(filename)
+
+  end
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     binding.pry
     self.artist.add_song(self)
-
   end
 
 end
