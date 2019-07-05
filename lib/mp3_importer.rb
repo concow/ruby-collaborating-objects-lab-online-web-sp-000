832fbd7315e2e94a47aeb65c1af37107c831a8d3
Connor Cowan <ccowan003@comcast.net>
2019-07-05 21:15:37 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,8 +5,10 @@ class MP3Importer
   def initialize(path)        #Whenever we create new instance of MP3Importer
     @path = path              #Set attribute path = to filename we initialize with
   end
-
+   #Had to google for Dir method
   def files
-
+    Dir.entries(path).select {|entry| entry.include?(".mp3")}       #select returns an array itself
   end
+
+  def import
 end
