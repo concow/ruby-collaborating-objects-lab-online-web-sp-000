39c4cb51999a4fc5a5f052242b58735d3734f953
Connor Cowan <ccowan003@comcast.net>
2019-07-05 21:57:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
   #sets the artist object to belong to the song
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
+    song_name = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
