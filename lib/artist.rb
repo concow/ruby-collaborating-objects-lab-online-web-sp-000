ef2113e78d479b1763f5d4a28a0abffe75973ad0
Connor Cowan <ccowan003@comcast.net>
2019-07-05 22:02:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,6 +26,7 @@ class Artist
   def self.find_or_create_by_name(name)                         #creates an artist by name maining uniqueness of objects by name property
     self.all.find {|artist| artist.name == name} || self.new(name).save
     #self.find(name) ? self.find(name) : self.create(name)
+  end
   def self.find(name)
       self.all.find {|artist| artist.name == name }
   end
